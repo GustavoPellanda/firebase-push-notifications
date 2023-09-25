795f54908d2ef9595af0ea69fc5ab0f7825568cd
Gustavo Pellanda <gusfipe@gmail.com>
2023-09-25 03:36:41 -0300

    service worker adicionado

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from './App';
 import { StartsFirebase } from './firebase/Firebase';
 
-StartsFirebase();
+const messaging = StartsFirebase();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/firebase-messaging-sw.js')
+    .then((registration) => {
+      messaging.useServiceWorker(registration);
+      console.log('Service Worker registered with Firebase Messaging.');
+    })
+    .catch((error) => {
+      console.error('Error registering Service Worker:', error);
+    });
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
