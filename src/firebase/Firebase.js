cbc24206a459f5909f7df074cdd0af3ebdd3e9f0
Gustavo Pellanda <gusfipe@gmail.com>
2023-09-25 03:08:47 -0300

    botão funcionando

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
-import { getMessaging, getToken } from 'firebase/messaging';
+import { getMessaging } from 'firebase/messaging';
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyAzhz1dbTgVY643pd4RTpn7i75RFSSwsqQ",
   authDomain: "fir-push-notifications-7fe2d.firebaseapp.com",
   projectId: "fir-push-notifications-7fe2d",
@@ -10,26 +10,8 @@ const firebaseConfig = {
   appId: "1:605977833285:web:31eb4b97b426f91e0b982c"
 };
 
-export const StartsFirebase = async () => {
-  // Initialize the Firebase app
+export const StartsFirebase = () => {
   const app = initializeApp(firebaseConfig);
-
-  // Get the messaging instance
   const messaging = getMessaging(app);
-
-  try {
-    // Request permission to send notifications
-    const permission = await Notification.requestPermission();
-    if (permission === 'granted') {
-      // Get the registration token for the current user
-      const token = await getToken(messaging);
-
-      // Now you can use the token to send notifications to this user
-      console.log('Firebase Cloud Messaging token:', token);
-    } else {
-      console.error('Permission denied for notifications');
-    }
-  } catch (error) {
-    console.error('Error getting Firebase Cloud Messaging token:', error);
-  }
+  return messaging;
 };
--- src/services/NotificationPermission.js
+++ src/services/NotificationPermission.js
@@ -1,15 +1,23 @@
-import { getMessaging } from 'firebase/messaging';
-
 // Asks permission from the user to send notifications
+
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken } from 'firebase/messaging';
+import { firebaseConfig } from '../firebase/Firebase';
 
 export const AsksForPermission = async () => {
   try {
-    const messaging = getMessaging();
-    await messaging.requestPermission();
-    const token = await messaging.getToken();
-    console.log('User token:', token);
-    return token;
+    const app = initializeApp(firebaseConfig);
+    const messaging = getMessaging(app);
+
+    const permission = await Notification.requestPermission();
+    
+    if (permission === 'granted') {
+      const token = await getToken(messaging);
+      console.log('Firebase Cloud Messaging token:', token);
+    } else {
+      console.error('Permission denied for notifications');
+    }
   } catch (error) {
-    console.error(error);
+    console.error('Error getting Firebase Cloud Messaging token:', error);
   }
 };
