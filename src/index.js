378b511acfef30e026be08d052acd3e880c29444
Gustavo Pellanda <gusfipe@gmail.com>
2023-09-25 03:58:31 -0300

    botão permissão funcionando

--- src/index.js
+++ src/index.js
@@ -3,19 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from './App';
 import { StartsFirebase } from './firebase/Firebase';
 
-const messaging = StartsFirebase();
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('./firebase/firebase-messaging-sw.js')
-    .then((registration) => {
-      messaging.useServiceWorker(registration);
-      console.log('Service Worker registered with Firebase Messaging.');
-    })
-    .catch((error) => {
-      console.error('Error registering Service Worker:', error);
-    });
-}
+StartsFirebase();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
